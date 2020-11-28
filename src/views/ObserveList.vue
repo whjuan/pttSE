8daf88921a73775ba9b2e022483e499412641240
whjuan <geniu723@gmail.com>
2020-11-29 00:21:05 +0800

    add ObserveTable component

--- src/views/ObserveList.vue
+++ src/views/ObserveList.vue
@@ -12,49 +12,13 @@
         :buttonText="buttonText">
       </GetInput>
 
-      <!--<form>
-        <div class="row mt-5">
-          <div class="col-10 pr-0">
-            <input type="text" class="form-control" placeholder="輸入ID" v-model="id">
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-primary" v-on:click="addRow(id)">新增觀察</button>
-          </div>
-         
-        </div>
-      </form>-->
-
       <br>
       <br>
 
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>觀察ID</th>
-            <th>操作</th>
-          </tr>
-        </thead>
-      
-
-        <tbody>
-          <tr v-for="id in obs_id" :key="id">
-            <td>
-              {{ id }}
-            </td>
-            <td>
-              <div>
-                <router-link :to="{ name: 'ViewRecords', params: { id: id } }" v-on:click="passValue(id)">查看紀錄</router-link>
-                &emsp;&emsp;&emsp;
-                <button type="button" class="btn btn-danger" v-on:click="deleteRow(id)">移除觀察</button>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-
-      </table>
-
-
-    
+      <ObserveTable
+        @deleteRow="deleteRow"
+        :obs_id="obs_id">
+      </ObserveTable>
     
    </div> 
   </div> 
@@ -67,27 +31,30 @@
 </style>
 
 <script>
-import GetInput from "@/components/GetInput.vue"
-var rows = []
+  import GetInput from "@/components/GetInput.vue"
+  import ObserveTable from "@/components/ObserveTable.vue"
+
+  var rows = []
 
   export default {
     name: 'ObserveList',
     components: { 
       GetInput,
+      ObserveTable,
     },
     data() {
       return {
         obs_id: rows,
-        id: '',
         inputPlaceholder: '輸入ID', //  GetInput components
         buttonText: '新增觀察', //  GetInput components
+        id: '', //  GetInput components
       }
     },
     methods:{
       getInput(data) {  //  GetInput components
         this.id = data;
 
-        if(this.id == undefined){ //  確認有沒有此 ID
+        if(this.id == undefined || this.id == ''){ //  確認有沒有此 ID
           alert('此 ID 不存在');
         }
         else if(rows.includes(this.id)){
@@ -98,17 +65,6 @@ var rows = []
         }
 
       },
-      /*addRow(id) {
-        if(id == undefined){ //  確認有沒有此 ID
-          alert('此 ID 不存在');
-        }
-        else if(rows.includes(id)){
-          alert('此 ID 已在觀察名單內');
-        }
-        else{
-          rows.push(id);
-        }
-      },*/
       deleteRow(id) {
         var yes = confirm('確定移除 '+ id +' 嗎？');  //  確認是否刪除
         if(yes){
@@ -120,9 +76,6 @@ var rows = []
           }
         }
       },
-      passValue(id) {
-        this.$router.push({name:'ViewRecords',params:{id:id}});
-      }
     }
   }
  
